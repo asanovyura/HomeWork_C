6483299ab15cf4ff32749287365c6728c4a1b4f5
Asanov Yura <asanovyura@icloud.com>
2023-06-09 17:16:48 +0300

    task 6

--- home_work.cs
+++ home_work.cs
@@ -19,7 +19,7 @@ else
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("enter first number");
+/*Console.WriteLine("enter first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("enter second number");
@@ -42,3 +42,14 @@ if (num3 >= max)
     max = num3;
 }
 Console.Write("max numer is " + max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = num % 2;
+if (x == 0)
+{
+    Console.Write("this number is integer");
+}
+else Console.Write("this number isn't integer");
