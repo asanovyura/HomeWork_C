926366d8cd85bf1520713d93946e53ca332d843c
Asanov Yura <asanovyura@icloud.com>
2023-06-09 17:08:46 +0300

    task №4

--- home_work.cs
+++ home_work.cs
@@ -1,6 +1,6 @@
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("enter first number");
+/*Console.WriteLine("enter first number");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("enter second number");
@@ -15,3 +15,30 @@ else
     Console.Write(num1 + " is max number ");
     Console.Write(num2 + " is min number ");
 }
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+if (num1 >= max)
+{
+    max = num1;
+}
+if (num2 >= max)
+{
+    max = num2;
+}
+if (num3 >= max)
+{
+    max = num3;
+}
+Console.Write("max numer is " + max);
