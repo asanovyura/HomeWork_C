b8f7f854f9bf3a1d91e878cc8bbb76727e25f74e
Asanov Yura <asanovyura@icloud.com>
2023-06-13 16:32:20 +0300

    task 13

--- home_work2.cs
+++ home_work2.cs
@@ -1,7 +1,23 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
 System.Console.WriteLine("введите число от 100 до 999");
 int num = Convert.ToInt32(Console.ReadLine());
     int a = num % 100;
     int b = a / 10;
     System.Console.WriteLine($"вторая цифра в введенном числе это {b} ");
+*/
 
+
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string text = Convert.ToString(num);
+if (text.Length > 2)
+{
+    Console.WriteLine($"третья цифра введенного числа - {text[2]}");
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
