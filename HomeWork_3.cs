97188c8326bac75a533d2c00336c80a204c8aba9
Asanov Yura <asanovyura@icloud.com>
2023-06-16 16:40:08 +0300

    second task

--- HomeWork_3.cs
+++ HomeWork_3.cs
@@ -1,5 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-
+/*
 System.Console.WriteLine("введите пятизначное число");
 int num = System.Convert.ToInt32(System.Console.ReadLine());
 int a = num % 10;
@@ -18,3 +18,34 @@ else
 {
     System.Console.WriteLine("число не является палиндромом");
 }
+*/
+
+
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string Name_1, string Name_2)
+{
+    Console.Write($"Введите координату {Name_1} точки {Name_2}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double sLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {sLength}");
