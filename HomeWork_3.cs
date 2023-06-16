84c160afb3bd72b276499b2992e7c84cb9bd5f92
Asanov Yura <asanovyura@icloud.com>
2023-06-16 16:20:44 +0300

    first task

--- HomeWork_3.cs
+++ HomeWork_3.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+System.Console.WriteLine("введите пятизначное число");
+int num = System.Convert.ToInt32(System.Console.ReadLine());
+int a = num % 10;
+int b = num / 10000;
+int c = num % 100 / 10;
+int d = num % 10000 / 1000;
+if (num < 10000 || num > 100000)
+{
+    System.Console.WriteLine("вы ввели не пятизначное число");
+}
+else if (a == b || c == d)
+{
+    System.Console.WriteLine("число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("число не является палиндромом");
+}
