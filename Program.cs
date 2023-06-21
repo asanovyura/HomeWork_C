2a6aec5c6ee6563eb53176c281b862434b56d987
Asanov Yura <asanovyura@icloud.com>
2023-06-21 13:36:44 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// task №1
 //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B без Math.Pow
-int Exponent (int numA , int numB)
+/*int Exponent (int numA , int numB)
 {
     int result = 1;
     for (int i = 1; i <= numB; i ++)
@@ -15,5 +15,27 @@ Console.WriteLine("введите число B");
 int numB = Convert.ToInt32(Console.ReadLine());
 int exponential = Exponent (numA , numB);
 Console.WriteLine($"получается {exponential}");
+*/
 
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int userNum){
+    
+    int count = Convert.ToString(userNum).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i <= count; i++){
+      advance = userNum - userNum % 10;
+      result = result + (userNum - advance);
+      userNum = userNum / 10;
+    }
+   return result;
+  }
+
+int sumNumbers = SumNumber(userNum);
+Console.WriteLine($"Сумма цифр {sumNumbers}");
 
