9341a7d37a4651f7d10af105a92aeb781617ab53
Asanov Yura <asanovyura@icloud.com>
2023-06-21 13:27:09 +0300

    task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task №1
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B без Math.Pow
+int Exponent (int numA , int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i ++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+Console.WriteLine("введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+int exponential = Exponent (numA , numB);
+Console.WriteLine($"получается {exponential}");
+
+
