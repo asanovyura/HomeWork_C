abf5fcf670c7398df438eeb149509c883e609b15
Asanov Yura <asanovyura@icloud.com>
2023-06-28 16:17:53 +0300

    1 task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.Write($"Введи колличество чисел для провеки");
+int a = System.Convert.ToInt32(System.Console.ReadLine());
+int[] massiveNumbers = new int[a];
+
+void InputNumbers(int a)
+{
+for (int i = 0; i < a; i++)
+  {
+    System
+    .Console.Write($"Введите {i+1} число: ");
+    massiveNumbers[i] = System.Convert.ToInt32(System.Console.ReadLine());
+  }
+}
+
+
+int result(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(a);
+
+System.Console.WriteLine($"Введено чисел больше 0: {result(massiveNumbers)} ");
