70dbf223861933e488626db40b1d1687dd8ce4cb
Asanov Yura <asanovyura@icloud.com>
2023-06-28 12:20:03 +0300

    task 3

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ void printArray(int[] numbers)
 //Задача 36: Задайте одномерный массив, заполненный случайными числами.
  //Найдите сумму элементов, стоящих на нечётных позициях
 
-Console.WriteLine("Введите размер массива");
+/*Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
@@ -66,4 +66,47 @@ void PrintArray(int[] numbers)
         }
     Console.WriteLine();
 }
+*/
 
+//Задача 38: Задайте массив вещественных чисел.
+ //Найдите разницу между максимальным и минимальным элементов массива.
+
+ Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000));
+        }
+}
+void PrintArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.WriteLine();
+}
