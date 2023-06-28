9c5a09af8c855cab3e2f81685ec43165d29997cd
Asanov Yura <asanovyura@icloud.com>
2023-06-28 16:30:22 +0300

    2 task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿
-System.Console.Write($"Введи колличество чисел для провеки");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+/*System.Console.Write($"Введи колличество чисел для провеки");
 int a = System.Convert.ToInt32(System.Console.ReadLine());
 int[] massiveNumbers = new int[a];
 
@@ -27,3 +27,50 @@ int result(int[] massiveNumbers)
 InputNumbers(a);
 
 System.Console.WriteLine($"Введено чисел больше 0: {result(massiveNumbers)} ");
+*/
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int[,] numbers = new int[2, 2];
+int[] point = new int[2];
+
+void InputNumbers()
+{
+  for (int i = 0; i < numbers.GetLength(0); i++)
+  {
+    Console.Write($"Введите значение {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите значение k: ");
+      else Console.Write($"Введите значение b: ");
+      numbers[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+int[] Decision(int[,] numbers)
+{
+  point[0] = (numbers[1,1] - numbers[0,1]) / (numbers[0,0] - numbers[1,0]);
+  point[1] = point[0] * numbers[0,0] + numbers[0,1];
+  return point;
+}
+
+void OutputResponse(int[,] numbers)
+{
+  if (numbers[0,0] == numbers[1,0] && numbers[0,1] == numbers[1,1]) 
+  {
+    Console.Write($"\n Прямые совпадают");
+  }
+  else if (numbers[0,0] == numbers[1,0] && numbers[0,1] != numbers[1,1]) 
+  {
+    Console.Write($"\n Прямые параллельны");
+  }
+  else 
+  {
+    Decision(numbers);
+    Console.Write($"\n Точка пересечения прямых: ({point[0]}, {point[1]})");
+  }
+}
+
+InputNumbers();
+OutputResponse(numbers);
