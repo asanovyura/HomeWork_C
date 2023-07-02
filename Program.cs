b20bbb179a316e52b915e4c79504ae288fc9e701
Asanov Yura <asanovyura@icloud.com>
2023-07-02 17:24:08 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
 System.Console.Write("Введите колличество строк: ");
 int m = System.Convert.ToInt32(System.Console.ReadLine());
 System.Console.Write("Введите колличество столбцов: ");
@@ -35,4 +36,49 @@ for (int i = 0; i < m; i++)
       }
       System.Console.WriteLine();
   }
+}
+*/
+
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("данный элемент отсутствует");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 101)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(""); 
+    }
 }
