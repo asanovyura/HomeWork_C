f9122d1b80e408dea27032fbe7915a4da7465a4b
Asanov Yura <asanovyura@icloud.com>
2023-07-04 16:49:01 +0300

    task 62

--- Program.cs
+++ Program.cs
@@ -1,61 +1,27 @@
-﻿// Задача 58: Задайте две матрицы. 
-//Напишите программу, которая будет находить произведение двух матриц.
+﻿//Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.WriteLine("введите колличество строк и столбцов в матрице ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] Matrix = new int[n, n];
 
-int rows = InputNumbers("Введите число строк 1-й матрицы: ");
-int columns1 = InputNumbers("Введите число столбцов 1-й матрицы : ");
-//столбцы в 1-й матрице = строкам 2-й матрицы
-int columns2 = InputNumbers("Введите число столбцов 2-й матрицы: ");
-int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+int temp = 1;
+int i = 0;
+int j = 0;
 
-int[,] firstMartrix = new int[rows, columns1];
-CreateArray(firstMartrix);
-Console.WriteLine($"\n Первая матрица:");
-WriteArray(firstMartrix);
-
-int[,] secomdMartrix = new int[columns1, columns2];
-CreateArray(secomdMartrix);
-Console.WriteLine($"\n Вторая матрица:");
-WriteArray(secomdMartrix);
-
-int[,] resultMatrix = new int[rows,columns2];
-
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-Console.WriteLine($"\n Произведение первой и второй матриц:");
-WriteArray(resultMatrix);
-
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
 {
-  for (int i = 0; i < resultMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatrix.GetLength(1); j++)
-    {
-      int sum = 0;
-      for (int k = 0; k < firstMartrix.GetLength(1); k++)
-      {
-        sum += firstMartrix[i,k] * secomdMartrix[k,j];
-      }
-      resultMatrix[i,j] = sum;
-    }
-  }
+  Matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= Matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > Matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
 }
 
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
+WriteArray(Matrix);
 
 void WriteArray (int[,] array)
 {
@@ -63,7 +29,10 @@ void WriteArray (int[,] array)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i,j] + " ");
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
     }
     Console.WriteLine();
   }
