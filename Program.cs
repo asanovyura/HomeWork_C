fc50c267099056108e71a7a734cf6b9407d1da78
Asanov Yura <asanovyura@icloud.com>
2023-07-07 15:07:50 +0300

    task № 64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int ShowRange(int n)
+{    
+    if (n == 0) return 1;
+        if (n - 1 == 0) System.Console.Write($"{n}");
+        else System.Console.Write($"{n}, ");        
+    return n - ShowRange(n - 1);
+}
+
+System.Console.WriteLine("Input n: ");
+int n = System.Convert.ToInt32(System.Console.ReadLine());
+ShowRange(n);
